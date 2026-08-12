b834a19d944cc3691b438b45ea9eca96a674672a
Myroslava-Hnatiuk <myroslava.hnatiuk@ukr.net>
2026-08-12 20:20:05 +0100

    fix coments

--- classes.py
+++ classes.py
@@ -34,11 +34,14 @@ class Record:
     def edit_phone(self, old_phone, new_phone):
         for p in self.phones:
             if p.value == old_phone:
-                p.value = new_phone
-                break
+                p.value = Phone(new_phone)
 
     def find_phone(self, phone):
-        return [p for p in self.phones if p.value == phone]
+        for p in self.phones:
+            if p.value == phone:
+                return p
+            else:
+                None
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
